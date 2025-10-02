ee9ce178fedac5873b474a8bb98e259cfefa89b6
chetnadua008 <chetnadua36@gmail.com>
2025-10-02 15:54:28 +0530

    controlled input in login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,98 +2,121 @@
 
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
 } from "@/components/ui/tabs"
 
 
-const Login = ()=>{
-  return (
-    <div className="flex justify-center min-h-screen bg-gray-100">
-        <div className="flex w-full max-w-sm flex-col gap-6">
+import { useState } from "react"
+const Login = () => {
+    //state
+    const [signupInput, setSignupInput] = useState({ name: "", email: "", password: "" });
+    const [loginInput, setLoginInput] = useState({ email: "", password: "" });
 
-      <Tabs defaultValue="login">
+    //update state when form data change
+    //bonus step: convert email to lowercase 
+    const inputChangeHandler = (e, type) => {
+        const { name, value } = e.target;
+        const cleanValue = name === "email" ? value.trim().toLowerCase() : value;
 
-        <TabsList>
-          <TabsTrigger value="login">login</TabsTrigger>
-          <TabsTrigger value="signup">signup</TabsTrigger>
-        </TabsList>
+        if (type === 'signup') {
+            setSignupInput({ ...signupInput, [name]: cleanValue });
+        } else {
+            setLoginInput({ ...loginInput, [name]: cleanValue });
+        }
+    }
 
-        <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>login</CardTitle>
-              <CardDescription>
-                Enter your credentials if you have already registered.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
+    //clicks signup/login button-> console the state
+    const submitHandler = (type) => {
+        const input = type === 'login' ? loginInput : signupInput;
+        console.log(input)
+    }
 
-               <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input type="email" placeholder="abc@gmail.com" required="true"/>
-              </div>
+    return (
+        <div className="flex justify-center min-h-screen bg-gray-100">
+            <div className="flex w-full max-w-sm flex-col gap-6">
 
-               <div className="grid gap-3">
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" placeholder="########" required="true"/>
-              </div>
+                <Tabs defaultValue="login">
 
+                    <TabsList>
+                        <TabsTrigger value="login">login</TabsTrigger>
+                        <TabsTrigger value="signup">signup</TabsTrigger>
+                    </TabsList>
 
-            </CardContent>
-            <CardFooter>
-              <Button>Login</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
+                    <TabsContent value="login">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>login</CardTitle>
+                                <CardDescription>
+                                    Enter your credentials if you have already registered.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="grid gap-6">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input name="email" value={loginInput.email} onChange={(e) => inputChangeHandler(e, "login")} type="email" placeholder="abc@gmail.com" required />
+                                </div>
 
-        <TabsContent value="signup">
-          <Card>
-            <CardHeader>
-              <CardTitle>signup</CardTitle>
-              <CardDescription>
-                Enter Sign up details for new registration.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6">
+                                <div className="grid gap-3">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input name="password" value={loginInput.password} onChange={(e) => inputChangeHandler(e, "login")} type="password" placeholder="########" required />
+                                </div>
 
-              <div className="grid gap-3">
-                <Label htmlFor="name">Name</Label>
-                <Input type="text" placeholder="Chetna Dua" required="true" />
-              </div>
+                            </CardContent>
 
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input type="email" placeholder="abc@gmail.com" required="true"/>
-              </div>
+                            <CardFooter>
+                                <Button onClick={() => submitHandler("login")}>Login</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
 
-               <div className="grid gap-3">
-                <Label htmlFor="password">Password</Label>
-                <Input type="password" placeholder="########" required="true"/>
-              </div>
+                    <TabsContent value="signup">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>signup</CardTitle>
+                                <CardDescription>
+                                    Enter Sign up details for new registration.
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent className="grid gap-6">
 
-            </CardContent>
-            <CardFooter>
-              <Button>Signup</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
-    </div>
-    </div>
-     
-  )
+                                <div className="grid gap-3">
+                                    <Label htmlFor="name">Name</Label>
+                                    <Input name="name" value={signupInput.name} onChange={(e) => inputChangeHandler(e, "signup")} type="text" placeholder="Chetna Dua" required />
+                                </div>
+
+                                <div className="grid gap-3">
+                                    <Label htmlFor="email">Email</Label>
+                                    <Input name="email" value={signupInput.email} onChange={(e) => inputChangeHandler(e, "signup")} type="email" placeholder="abc@gmail.com" required />
+                                </div>
+
+                                <div className="grid gap-3">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Input name="password" value={signupInput.password} onChange={(e) => inputChangeHandler(e, "signup")} type="password" placeholder="########" required />
+                                </div>
+
+                            </CardContent>
+                            <CardFooter>
+                                <Button onClick={() => submitHandler("signup")}>Signup</Button>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
+            </div>
+        </div>
+
+    )
 }
 export default Login;
