53c41bd2ee81f31b7e85f99863852abb7f6afc25
chetnadua008 <chetnadua36@gmail.com>
2025-12-26 18:01:25 +0530

    homepage url on navbar icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -50,10 +50,10 @@ const Navbar = () => {
         <div className='h-16 dark:bg-[#0A0A0A] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10'>
             {/* desktop */}
             <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
-                <div className="flex items-center gap-2">
+                <Link to="/"><div className="flex items-center gap-2">
                     <School size={"30"} />
                     <h1 className='hidden md:block font-extrabold text-2xl'>E-Learning</h1>
-                </div>
+                </div></Link>
                 {/* user icon and dark mode icon */}
                 <div className="flex items-center gap-8">
                     {
